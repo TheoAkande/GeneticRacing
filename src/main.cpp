94f8d1676243b93f3ec791859fad3c4e59f105d5
Theo Akande <ta722@ic.ac.uk>
2024-08-29 14:37:28 +0100

    feat: more tracks

--- src/main.cpp
+++ src/main.cpp
@@ -79,7 +79,7 @@ glm::mat4 viewMat;
 float appliedForce, totalForce, airResistance;
 float appliedTurning, totalTurning;
 
-const char *track = "assets/tracks/track1.tr";
+const char *track = "assets/tracks/track3.tr";
 vector<float> insideTrack;
 vector<float> outsideTrack;
 float trackStartLine[4];
