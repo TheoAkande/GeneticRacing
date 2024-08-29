7fd89b81c1c0ec8625de5d9650b4650d9408f27e
Theo Akande <ta722@ic.ac.uk>
2024-08-29 10:14:12 +0100

    feat: track limit collisions in cs

--- src/shaders/carPhysicsCS.glsl
+++ src/shaders/carPhysicsCS.glsl
@@ -21,9 +21,14 @@ layout(local_size_x = 1) in;
 layout(binding = 0) buffer inputBuffer1 { float carData[]; };
 layout(binding = 1) buffer inputBuffer2 { float carInputs[]; };
 layout(binding = 2) buffer outputBuffer {float carOutputs[]; };
+layout(binding = 3) buffer inputBuffer3 {float insideTrack[]; };
+layout(binding = 4) buffer inputBuffer4 {float outsideTrack[]; };
+layout(binding = 5) buffer outputBuffer2 {float carCollisions[]; };
 
 uniform int numCarFloats;
 uniform int numInputs;
+uniform int numInsideTrackPoints;
+uniform int numOutsideTrackPoints;
 
 uniform float engineForce;
 uniform float brakeForce;
@@ -32,6 +37,75 @@ uniform float maxSpeed;
 uniform float carMass;
 uniform float deltaTime;
 
+struct Line {
+    vec2 p1;
+    vec2 p2;
+};
+
+bool almostEqual(float a, float b) {
+    float epsilon = 0.000001;
+    return abs(a - b) <= epsilon;
+}
+
+float fmax(float a, float b) {
+    return a > b ? a : b;
+}
+
+float fmin(float a, float b) {
+    return a < b ? a : b;
+}
+
+// 0 -> collinear
+// 1 -> clockwise
+// 2 -> counterclockwise
+int orientation(vec2 p, vec2 q, vec2 r) {
+    float val = (q.y - p.y) * (r.x - q.x) - (q.x - p.x) * (r.y - q.y);
+    if (almostEqual(val, 0.0)) return 0;
+    return (val > 0) ? 1 : 2;
+}
+
+bool on_segment(vec2 p, vec2 q, vec2 r) {
+    return q.x <= fmax(p.x, r.x) && q.x >= fmin(p.x, r.x) && q.y <= fmax(p.y, r.y) && q.y >= fmin(p.y, r.y);
+}
+
+vec2 order(float n1, float n2) {
+    if (n1 < n2) {
+        return vec2(n1, n2);
+    } else {
+        return vec2(n2, n1);
+    }
+}
+
+// Check if bounding boxes of line and particle intersect
+bool boundingIntersect(Line l1, Line l2) {
+    vec2 ordered_1 = order(l1.p1.x, l1.p2.x);
+    vec2 ordered_2 = order(l2.p1.x, l2.p2.x);
+    vec2 ordered_3 = order(l1.p1.y, l1.p2.y);
+    vec2 ordered_4 = order(l2.p1.y, l2.p2.y);
+
+    return ordered_1.x <= ordered_2.y && ordered_1.y >= ordered_2.x && ordered_3.x <= ordered_4.y && ordered_3.y >= ordered_4.x;
+}
+
+bool intersect(Line l1, Line l2) {
+    if (!boundingIntersect(l1, l2)) {   // easily prune most cases
+        return false;
+    }
+    int o1 = orientation(l1.p1, l1.p2, l2.p1);
+    int o2 = orientation(l1.p1, l1.p2, l2.p2);
+    int o3 = orientation(l2.p1, l2.p2, l1.p1);
+    int o4 = orientation(l2.p1, l2.p2, l1.p2);
+
+    if (o1 != o2 && o3 != o4) {
+        return true;
+    }
+
+    if (o1 == 0 && on_segment(l1.p1, l2.p1, l1.p2)) return true;
+    if (o2 == 0 && on_segment(l1.p1, l2.p2, l1.p2)) return true;
+    if (o3 == 0 && on_segment(l2.p1, l1.p1, l2.p2)) return true;
+    if (o4 == 0 && on_segment(l2.p1, l1.p2, l2.p2)) return true;
+
+    return false;
+}
 
 void main()
 {
@@ -65,6 +139,9 @@ void main()
     acceleration = totalForce / carMass;
     angle += totalTurning * deltaTime;
     speed += acceleration * deltaTime;
+
+    float oldX = x;
+    float oldY = y;
     x += speed * cos(angle) * deltaTime;
     y += speed * sin(angle) * deltaTime;
 
@@ -73,4 +150,23 @@ void main()
     carOutputs[in1Index + 2] = angle;
     carOutputs[in1Index + 3] = speed;
     carOutputs[in1Index + 4] = acceleration;
+
+    Line carLine = Line(vec2(oldX, oldY), vec2(x, y));
+    carCollisions[index] = 0.0;
+    for (uint i = 0; i < numInsideTrackPoints; i++) {
+        uint inIndex = i * 2;
+        Line trackLine = Line(vec2(insideTrack[inIndex], insideTrack[inIndex + 1]), vec2(insideTrack[(inIndex + 2) % (numInsideTrackPoints * 2)], insideTrack[(inIndex + 3) % (numInsideTrackPoints * 2)]));
+        if (intersect(carLine, trackLine)) {
+            carCollisions[index] = 1.0;
+            break;
+        }
+    }
+    for (uint i = 0; i < numInsideTrackPoints; i++) {
+        uint inIndex = i * 2;
+        Line trackLine = Line(vec2(outsideTrack[inIndex], outsideTrack[inIndex + 1]), vec2(outsideTrack[(inIndex + 2) % (numoutsideTrackPoints * 2)], outsideTrack[(inIndex + 3) % (numoutsideTrackPoints * 2)]));
+        if (intersect(carLine, trackLine)) {
+            carCollisions[index] = 1.0;
+            break;
+        }
+    }
 }
