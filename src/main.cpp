32caced7454fb7d894befca079121649aafe4c1f
Theo Akande <ta722@ic.ac.uk>
2024-08-30 19:17:25 +0100

    feat: move definitions into global header file

--- src/main.cpp
+++ src/main.cpp
@@ -13,42 +13,9 @@
 
 #include "Utils.h"
 #include "TrackMaker.h"
+#include "Defs.h"
 
 using namespace std;
-
-#define deterministic false
-#define deterministicDt 0.015l
-
-// OpenGL definitions
-#define numVBOs 6
-#define numVAOs 1
-#define numCBs 12
-#define windowWidth 2000
-#define windowHeight 1500
-#define numCars 2
-#define numCarFloats 9
-
-// Car definitions
-#define carWidth 0.02f
-#define carHeight 0.03f
-
-#define numInputs 5
-
-// Physics definitions
-#define frictionMax 0.05f
-#define carMass 1.0f
-#define carForce 0.1f
-#define breakingForce 0.3f
-#define vMax 1.0f
-#define maxTurningRate 1.2f
-
-#define numComputerVisionAngles 15
-
-#define calculateDistanceInterval 60
-
-#define MAX_FLOATS 268435456
-#define MAX_SSBO_SIZE 1073741824
-#define MAX_SSBO 16
 
 GLuint vao[numVAOs];
 GLuint vbo[numVBOs];
