924444899c170a20775ca9b15d6d3f631f2c783f
Theo Akande <ta722@ic.ac.uk>
2024-08-30 19:17:35 +0100

    feat: skeleton for genetic neural networks

---
+++ src/DeepNeuralNets.h
@@ -0,0 +1,85 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cmath>
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <SOIL2/soil2.h>
+#include <cstdlib>
+
+#include "Defs.h"
+
+using namespace std;
+
+#ifndef DEEPNEURALNETS_H
+#define DEEPNEURALNETS_H
+
+#define MAX_FLOATS 268435456
+#define MAX_SSBO_SIZE 1073741824
+#define MAX_SSBO 16
+
+#define NUM_NEURAL_NETS numCars
+
+// Currently not an RNN -> if rnn then add numInputs to NUM_INPUTS
+#define NUM_INPUTS (numComputerVisionAngles + numCarFloats)
+#define NUM_OUTPUTS numInputs
+
+#define NUM_HIDDEN_LAYER_1_NODES 32
+#define NUM_HIDDEN_LAYER_2_NODES 16
+#define NUM_HIDDEN_LAYER_3_NODES 8
+
+#define NUM_GENERATION_LEADERS 10
+
+#define NUM_NN_CBS 11
+
+class DeepNeuralNets
+{
+    private:
+        // Compute shader variables
+        static GLuint neuralNetComputeShader;
+        static GLuint evolutionComputeShader;
+        static GLuint nnCBOs[NUM_NN_CBS];
+
+        // Network inputs
+        static float *carData;
+        static float *computerVisionData;
+
+        // Neural network weights
+        static float layer1Weights[(NUM_INPUTS * NUM_HIDDEN_LAYER_1_NODES + 1) * NUM_NEURAL_NETS];
+        static float layer2Weights[(NUM_HIDDEN_LAYER_1_NODES * NUM_HIDDEN_LAYER_2_NODES + 1) * NUM_NEURAL_NETS];
+        static float layer3Weights[(NUM_HIDDEN_LAYER_2_NODES * NUM_HIDDEN_LAYER_3_NODES + 1) * NUM_NEURAL_NETS];
+        static float outputWeights[(NUM_HIDDEN_LAYER_3_NODES * NUM_OUTPUTS + 1) * NUM_NEURAL_NETS];
+
+        // Neural network outputs
+        static float layer1Outputs[NUM_HIDDEN_LAYER_1_NODES * NUM_NEURAL_NETS];
+        static float layer2Outputs[NUM_HIDDEN_LAYER_2_NODES * NUM_NEURAL_NETS];
+        static float layer3Outputs[NUM_HIDDEN_LAYER_3_NODES * NUM_NEURAL_NETS];
+        static float outputOutputs[NUM_OUTPUTS * NUM_NEURAL_NETS];
+
+        // Neural network fitness
+        static float fitness[NUM_NEURAL_NETS];
+
+        // Generation leaders
+        static float genLeadersLayer1Weights[(NUM_INPUTS * NUM_HIDDEN_LAYER_1_NODES + 1) * NUM_GENERATION_LEADERS];
+        static float genLeadersLayer2Weights[(NUM_HIDDEN_LAYER_1_NODES * NUM_HIDDEN_LAYER_2_NODES + 1) * NUM_GENERATION_LEADERS];
+        static float genLeadersLayer3Weights[(NUM_HIDDEN_LAYER_2_NODES * NUM_HIDDEN_LAYER_3_NODES + 1) * NUM_GENERATION_LEADERS];
+        static float genLeadersOutputWeights[(NUM_HIDDEN_LAYER_3_NODES * NUM_OUTPUTS + 1) * NUM_GENERATION_LEADERS];
+        static float genLeadersFitness[NUM_GENERATION_LEADERS];
+
+        // Evolution
+        static void gatherGenerationLeaders(void);
+        static void evolveGenerationLeaders(void);
+    public:
+        DeepNeuralNets();
+
+        static void initNeuralNets(float *carData, float *computerVisionData); // pointers to buffers that will be used as inputs
+        static float *invokeNeuralNets(void); // return pointer to outputOutputs
+        static void setFitness(float *fitnessData); // inform neural nets of their fitness
+        static void evolveNeuralNets(void); // evolve neural nets
+};
+
+#endif
