224a8305e24e0d6fe3fe995ac1d0b00c34b21d2d
Theo Akande <ta722@ic.ac.uk>
2024-09-06 15:09:53 +0100

    feat: fitness compute shader that calculates interpolated number of gates moved

--- src/shaders/fitnessCS.glsl
+++ src/shaders/fitnessCS.glsl
@@ -3,15 +3,44 @@
 layout(local_size_x = 1) in;
 
 layout (binding = 0) buffer inputBuffer1 { float carData[]; };
-layout (binding = 1) buffer inputBuffer2 { int carLaps[]; };
-layout (binding = 2) buffer outputBuffer {float fitness[]; };
+layout (binding = 1) buffer inputBuffer2 { float carEval[]; };
+layout (binding = 2) buffer inputBuffer3 {float gates[]; };
+layout (binding = 3) buffer outputBuffer {float fitness[]; };
 
 uniform int numCarFloats;
+uniform int numEvalFloats;
+uniform int numGates;
 
 void main()
 {
     uint index = gl_GlobalInvocationID.x;
-    uint inIndex = index * numCarFloats;
     
-    fitness[index] = carData[inIndex + 8] + float(carLaps[index]) * 50.0;
+    // Number of gates passed
+    float gatesPassed = float(int(carEval[index * numEvalFloats + 1])) - 1;
+
+    int gateIndex = int(carEval[index * numEvalFloats]);
+    int lastGate = gateIndex == 0 ? numGates - 1 : gateIndex - 1;
+
+    // Distance from last gate to next
+    float distanceBetweenGates = length(
+        vec2(
+            gates[gateIndex * 3] - gates[lastGate * 3],
+            gates[gateIndex * 3 + 1] - gates[lastGate * 3 + 1]
+        )
+    );
+
+    // Distance from car to next gate
+    float distanceToNextGate = length(
+        vec2(
+            gates[gateIndex * 3] - carData[index * numCarFloats],
+            gates[gateIndex * 3 + 1] - carData[index * numCarFloats + 1]
+        )
+    );
+
+    // Interpolate distance to next gate
+    // Note: will be negative if further away that last gate. That is intended
+    float interpolatedDistance = distanceToNextGate / distanceBetweenGates;
+    float distanceFitness = 1.0 - interpolatedDistance;
+    
+    fitness[index] = gatesPassed + distanceFitness;
 }
