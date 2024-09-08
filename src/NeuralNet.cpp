0c703dae0722d989617a0e299ffafe1878219d59
Theo Akande <ta722@ic.ac.uk>
2024-09-08 14:46:16 +0100

    refactor: use this-> syntax

--- src/NeuralNet.cpp
+++ src/NeuralNet.cpp
@@ -1,5 +1,4 @@
 #include "NeuralNet.h"
-#include "Utils.h"
 
 float FeedForwardNeuralNet::randomWeightRange = 1.0f;
 GLuint FeedForwardNeuralNet::invocationShader;
@@ -9,34 +8,34 @@ bool FeedForwardNeuralNet::initialized = false;
 
 void FeedForwardNeuralNet::setupArchitecture(void) {
     // Setup compute buffer objects
-    numCbs = architecture.size() * 2;   // 1 for weights, 1 for outputs per layer
-    cbs.resize(numCbs);
-    glGenBuffers(numCbs, cbs.data());
+    this->numCbs = this->architecture.size() * 2;   // 1 for weights, 1 for outputs per layer
+    this->cbs.resize(this->numCbs);
+    glGenBuffers(this->numCbs, this->cbs.data());
 
     // Setup weights and outputs
-    outputs.push_back(new vector<float>(architecture[0]));  // Inputs count as outputs of first layer
-    outputs[0]->resize(architecture[0]);
-    for (int i = 0; i < architecture.size() - 1; i++) {
-        weights.push_back(new vector<float>((architecture[i] + 1) * architecture[i + 1]));  // num weights = num inputs + 1 for bias
-        outputs.push_back(new vector<float>(architecture[i + 1]));
-        outputs[i + 1]->resize(architecture[i + 1]);
+    this->outputs.push_back(new vector<float>(this->architecture[0]));  // Inputs count as outputs of first layer
+    this->outputs[0]->resize(this->architecture[0]);
+    for (int i = 0; i < this->architecture.size() - 1; i++) {
+        this->weights.push_back(new vector<float>((this->architecture[i] + 1) * this->architecture[i + 1]));  // num weights = num inputs + 1 for bias
+        this->outputs.push_back(new vector<float>(this->architecture[i + 1]));
+        this->outputs[i + 1]->resize(this->architecture[i + 1]);
     }
 }
 
 void FeedForwardNeuralNet::createRandomWeights(void) {
     // Note: parallelize this later when I make better glsl random number generator
     // Generate weights
-    for (int i = 0; i < weights.size(); i++) {
-        for (int j = 0; j < weights[i]->size(); j++) {
-            (*weights[i])[j] = (float)rand() / (float)RAND_MAX * randomWeightRange * 2 - randomWeightRange;
+    for (int i = 0; i < this->weights.size(); i++) {
+        for (int j = 0; j < this->weights[i]->size(); j++) {
+            (*this->weights[i])[j] = (float)rand() / (float)RAND_MAX * randomWeightRange * 2 - randomWeightRange;
         }
     }
 
     // Load weights into compute buffer objects
-    for (int i = 0; i < weights.size(); i++) {
+    for (int i = 0; i < this->weights.size(); i++) {
         glBindBuffer(GL_SHADER_STORAGE_BUFFER, cbs[i * 2]);
         // Note: we use DYNAMIC_COPY because at the moment the outputs are going to be stored back to CPU each time
-        glBufferData(GL_SHADER_STORAGE_BUFFER, sizeof(float) * weights[i]->size(), weights[i]->data(), GL_DYNAMIC_COPY);
+        glBufferData(GL_SHADER_STORAGE_BUFFER, sizeof(float) * this->weights[i]->size(), this->weights[i]->data(), GL_DYNAMIC_COPY);
     }
 }
 
@@ -45,12 +44,12 @@ void FeedForwardNeuralNet::feedForward(int layer) {
     glUseProgram(invocationShader);
 
     // Bind the weights
-    glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 0, cbs[layer * 2]);      // Weights
-    glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 1, cbs[layer * 2 + 1]);  // Inputs
-    glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 2, cbs[layer * 2 + 3]);  // Outputs    
+    glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 0, this->cbs[layer * 2]);      // Weights
+    glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 1, this->cbs[layer * 2 + 1]);  // Inputs
+    glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 2, this->cbs[layer * 2 + 3]);  // Outputs    
 
     // Dispatch the compute shader
-    glDispatchCompute(architecture[layer + 1], 1, 1);
+    glDispatchCompute(this->architecture[layer + 1], 1, 1);
     glMemoryBarrier(GL_ALL_BARRIER_BITS);
 }
 
@@ -99,15 +98,15 @@ void FeedForwardNeuralNet::invoke(vector<float> *inputs, vector<float> *outputs)
     }
 
     // Load input data
-    glBindBuffer(GL_SHADER_STORAGE_BUFFER, cbs[1]);
+    glBindBuffer(GL_SHADER_STORAGE_BUFFER, this->cbs[1]);
     glBufferData(GL_SHADER_STORAGE_BUFFER, sizeof(float) * inputs->size(), inputs->data(), GL_DYNAMIC_DRAW);
     memcpy(this->outputs[0]->data(), inputs->data(), sizeof(float) * inputs->size());
 
-    for (int i = 0; i < weights.size(); i++) {
+    for (int i = 0; i < this->weights.size(); i++) {
         // Feed forward through the network
         feedForward(i); 
         // Get output
-        glBindBuffer(GL_SHADER_STORAGE_BUFFER, cbs[(i + 1) * 2 + 1]);
+        glBindBuffer(GL_SHADER_STORAGE_BUFFER, this->cbs[(i + 1) * 2 + 1]);
         glGetBufferSubData(GL_SHADER_STORAGE_BUFFER, 0, sizeof(float) * this->outputs[i + 1]->size(), this->outputs[i + 1]->data());
     }
 
@@ -117,9 +116,9 @@ void FeedForwardNeuralNet::invoke(vector<float> *inputs, vector<float> *outputs)
 }
 
 void FeedForwardNeuralNet::destroy(void) {
-    for (int i = 0; i < weights.size(); i++) {
-        delete weights[i];
-        delete outputs[i];
+    for (int i = 0; i < this->weights.size(); i++) {
+        delete this->weights[i];
+        delete this->outputs[i];
     }
-    glDeleteBuffers(numCbs, cbs.data());
+    glDeleteBuffers(this->numCbs, this->cbs.data());
 }
