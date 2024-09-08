7d6f1ac38fc782c2cb50314e709857ee0d878928
Theo Akande <ta722@ic.ac.uk>
2024-09-08 14:06:34 +0100

    feat: initial ffnn constructor

--- src/NeuralNet.cpp
+++ src/NeuralNet.cpp
@@ -1,3 +1,23 @@
 #include "NeuralNet.h"
 
 float FeedForwardNeuralNet::randomWeightRange = 1.0f;
+
+// Private
+
+
+
+
+
+// Public
+
+FeedForwardNeuralNet::FeedForwardNeuralNet(vector<int> architecture, uint64_t seed)
+{
+    this->architecture = architecture;
+    this->seed = seed;
+    this->initialized = false;
+
+    setupArchitecture();
+    createRandomWeights();
+
+    this->initialized = true;
+}
