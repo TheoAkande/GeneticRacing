be1110406bc12900ba3d353fc304855d713e52ef
Theo Akande <ta722@ic.ac.uk>
2024-09-16 14:28:19 +0100

    feat: invoke shader implementation

--- src/Matrix.cpp
+++ src/Matrix.cpp
@@ -6,6 +6,40 @@ GLuint Matrix::matCBOs[NUM_MATRIX_CBO];
 
 // Private
 
+void Matrix::invokeShader(GLuint shader, Matrix *m, int invokations) {
+    // Bind the shader
+    glUseProgram(shader);
+
+    // Check if it is a unary operation
+    bool unary = m == nullptr;
+
+    // Set the uniforms
+    GLuint uLoc = glGetUniformLocation(shader, "rowsA");
+    glUniform1i(uLoc, this->rows);
+    uLoc = glGetUniformLocation(shader, "colsA");
+    glUniform1i(uLoc, this->cols);
+
+    if (!unary) {
+        uLoc = glGetUniformLocation(shader, "rowsB");
+        glUniform1i(uLoc, m->rows);
+        uLoc = glGetUniformLocation(shader, "colsB");
+        glUniform1i(uLoc, m->cols);
+    }
+
+    // Bind the compute buffer objects
+    glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 0, matCBOs[0]);  // Input A
+    glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 1, matCBOs[1]);  // Output
+    if (!unary) {
+        glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 2, m->matCBOs[0]);  // Input B
+    }
+
+    // Dispatch the compute shader
+    glDispatchCompute(invokations, 1, 1);
+
+    // Wait for the shader to finish
+    glFinish();
+}
+
 void Matrix::setupClass(void) {
     if (Matrix::initialized) return;
 
